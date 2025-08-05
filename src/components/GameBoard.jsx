3919e8d89f7360e98d9a55eed581028bb9139251
dima tikhtey <uu36548@gmail.com>
2025-08-05 17:21:47 +0300

    first

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = ({ onSelectSquare, turns }) => {
+  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  //   function handleSelectSquare(rowIndex, colIndex) {
+  //     setGameBoard((prevGameBoard) => {
+  //       const updatedBoard = prevGameBoard.map((row) => [...row]);
+  //       if (updatedBoard[rowIndex][colIndex] !== null) return prevGameBoard;
+
+  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol; // допустим, пока всегда X
+  //       return updatedBoard;
+  //     });
+  //     onSelectSquare()
+  //   }
+  let gameBoard = initialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => {
+                    if (playerSymbol === null)
+                      onSelectSquare(rowIndex, colIndex);
+                  }}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+const Player = ({ initialName, symbol, isActive }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  function handleEditClick() {
+    setIsEditing((prev) => !prev);
+  }
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+  return (
+    <li className={isActive ? ' active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+};
+
+export default Player;
